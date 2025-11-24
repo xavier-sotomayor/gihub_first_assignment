b990c18dd74ddf531f99e51c4b631562f93c6954
xavier-sotomayor <xaviersotomayor@usf.edu>
2025-11-23 20:37:49 -0500

    Contingency for ValueError added

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ print("Welcome to my python program")
 sales = input("How many sales did you make today?")
 sales = float(sales)
 weekly_sales = sales * 7
+try:
+    sales = float(sales)
+except ValueError:
+    print("Enter a valid numer.")
+    exit()
 print(f"At this rate, you are likely to make {weekly_sales} sales this week.")
+
