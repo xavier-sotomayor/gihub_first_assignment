8d0d3afe72cd02894750032e0db46c09edb5d5c0
xavier-sotomayor <xaviersotomayor@usf.edu>
2025-11-23 20:33:31 -0500

    Print statement added

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my python program")
 sales = input("How many sales did you make today?")
 sales = float(sales)
 weekly_sales = sales * 7
+print(f"At this rate, you are likely to make {weekly_sales} sales this week.")
